2ed0373794ef043b93ab63f433a025b7375bef75
hyyyyyyy <460383541@qq.com>
2024-09-05 15:11:16 +0800

    test

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
 const fs = require('fs')
 const path = require('path')
+const { getInput, setFailed } = require('@actions/core')
+const { GitHub } = require('@actions/github')
 
-try {
-  // const ignore = core.getInput('ignore')
-  // const sender = github.context.sender
-  // const head_commit = github.context.head_commit
-  // const url = sender.url
-  // const author = head_commit.author
-  const file = fs.readdirSync(__dirname)
-  file.forEach((res) => {
-    const fullPath = path.join(__dirname, res)
-    const stat = fs.statSync(fullPath)
-    const filename = path.basename(res)
-    console.log(filename)
-  })
-} catch (error) {
-  core.setFailed(error.message)
+async function main() {
+  try {
+    async function searchDirectory(github, owner, repo, directory) {
+      try {
+        const branches = await github.repos.listBranches({ owner, repo })
+        const defaultBranch = branches.data.find(
+          (branch) => branch.name === 'master'
+        ).name
+        // 使用 git ls-tree 命令来查找目录中的文件
+        const listTree = await github.repos.getTree({
+          owner,
+          repo,
+          sha: defaultBranch,
+          recursive: '1',
+        })
+        const tree = listTree.data.tree
+        const matchingFiles = tree
+        if (!!directory) {
+          matchingFiles = tree.filter((file) => file.path.startsWith(directory))
+        }
+        matchingFiles.forEach((file) => console.log(file.path))
+      } catch (error) {
+        console.error('Error searching directory:', error)
+        process.exit(1)
+      }
+    }
+
+    // 在 GitHub Actions 工作流程中使用
+    const token = getInput('token')
+    const owner = getInput('owner')
+    const repo = getInput('repo')
+    const directory = getInput('directory')
+    const github = new GitHub(token)
+    searchDirectory(github, owner, repo, directory)
+  } catch (error) {
+    setFailed(error.message)
+  }
 }
+
+main()
