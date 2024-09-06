1ef200dea5c80475b2a5942ed2452fb78d4eb038
hyyyyyyy <460383541@qq.com>
2024-09-06 18:03:20 +0800

    完成

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# hyyy-toc-action
- 生成目录 readme
+## [.github](https://github.com/huangying1111/Hyyy-Learning-Record/tree/master/.github)
+
+* [README.md](https://github.com/huangying1111/Hyyy-Learning-Record/tree/master/README.md)
+
+## [听书看书笔记](https://github.com/huangying1111/Hyyy-Learning-Record/tree/master/听书看书笔记)
+
+## [绘画](https://github.com/huangying1111/Hyyy-Learning-Record/tree/master/绘画)
+
--- index.js
+++ index.js
@@ -1,46 +1,117 @@
-const { getInput, setFailed } = require('@actions/core')
-const { getOctokit } = require('@actions/github')
+const { getInput } = require('@actions/core')
+const { Octokit } = require('@octokit/core')
+class App {
+  _github
 
-async function main() {
-  try {
-    async function searchDirectory(github, owner, repo, directory) {
-      try {
-        const branches = await github.repos.listBranches({ owner, repo })
-        const defaultBranch = branches.data.find(
-          (branch) => branch.name === 'master'
-        ).name
-        // 使用 git ls-tree 命令来查找目录中的文件
-        const listTree = await github.repos.getTree({
-          owner,
-          repo,
-          sha: defaultBranch,
-          recursive: '1',
-        })
-        const tree = listTree.data.tree
-        const matchingFiles = tree
-        if (!!directory) {
-          matchingFiles = tree.filter((file) => file.path.startsWith(directory))
-        }
-        matchingFiles.forEach((file) => console.log(file.path))
-      } catch (error) {
-        console.error('Error searching directory:', error)
-        process.exit(1)
-      }
-    }
+  _owner
+  _repo
+  _token
 
+  _ignore
+  _updatePath
+  _commitMessage
+  _branch = 'master'
+  _tree
+
+  _headers = {
+    'X-GitHub-Api-Version': '2022-11-28',
+  }
+  constructor() {
     // 在 GitHub Actions 工作流程中使用
-    // const owner = 'huangying1111'
-    // const repo = 'Hyyy-Learning-Record'
-    // const directory = ''
-    const token = getInput('token')
-    const owner = getInput('owner')
-    const repo = getInput('repo')
-    const directory = getInput('directory')
-    const github = getOctokit(token)
-    searchDirectory(github, owner, repo, directory)
-  } catch (error) {
-    setFailed(error.message)
+    this._owner = getInput('owner')
+    this._repo = getInput('repo')
+    this._token = getInput('token')
+    this._ignore = getInput('ignore') ?? []
+    this._updatePath = getInput('updatePath')
+    this._commitMessage = getInput('commitMessage')
+
+    this._github = new Octokit({
+      auth: this._token,
+    })
+    this.init()
+  }
+  async init() {
+    try {
+      const branchResponse = await this._github.request(
+        'GET /repos/{owner}/{repo}/branches/{branch}',
+        {
+          owner: this._owner,
+          repo: this._repo,
+          branch: this._branch,
+          headers: this._headers,
+        }
+      )
+      const tree_sha = branchResponse.data.commit.sha
+      const menu = await this.getListTree(tree_sha)
+      let markdown = ''
+      markdown = this.toMarkDown(markdown, menu)
+      this.update(markdown)
+    } catch (error) {
+      console.error('Error:', error)
+      process.exit(1)
+    }
+  }
+  getListTree = async (tree_sha) => {
+    const getTreePath = await this._github.request(
+      'GET /repos/{owner}/{repo}/git/trees/{tree_sha}',
+      {
+        owner: this._owner,
+        repo: this._repo,
+        tree_sha: tree_sha,
+        recursive: 1,
+        headers: this._headers,
+      }
+    )
+    this._tree = getTreePath.data.tree
+    let matchingFiles = getTreePath.data.tree
+    if (this._ignore.length > 0) {
+      matchingFiles = matchingFiles.filter((file) =>
+        this._ignore.every((e) => !file.path.includes(e))
+      )
+    }
+    return matchingFiles
+  }
+
+  toMarkDown(markdown, data) {
+    data.forEach((item) => {
+      const namesArr = `/${item.path}`.split('/')
+      markdown += `${item.type == 'tree' ? '###' : '*'} [${
+        namesArr[namesArr.length - 1]
+      }](https://github.com/${this._owner}/${this._repo}/tree/master${
+        item.path
+      })\n\n`
+    })
+    return markdown
+  }
+
+  update = async (newContent) => {
+    // 获取当前文件内容
+    const data = await this._github.request(
+      'GET /repos/{owner}/{repo}/commits',
+      {
+        owner: this._owner,
+        repo: this._repo,
+        headers: this._headers,
+      }
+    )
+    const currentContent = data.data[0]
+    const sha = currentContent.sha
+    const content = Buffer.from(newContent).toString('base64')
+    const getPathSha = this._tree.filter((e) => {
+      return e.path == this._updatePath
+    })[0]['sha']
+    await this._github.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+      owner: this._owner,
+      repo: this._repo,
+      message: this._commitMessage,
+      parents: [sha],
+      path: this._updatePath,
+      committer: currentContent.commit.committer,
+      content: content,
+      headers: this._headers,
+      sha: getPathSha,
+    })
   }
 }
 
-main()
+new App()
